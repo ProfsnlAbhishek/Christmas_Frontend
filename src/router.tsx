c28796548f4033c0dc34e4840a7217609880eb20
ROBERT MURPHY <ROBERT.MURPHY@JFS.OHIO.GOV>
2026-08-18 08:42:06 -0400

    Merged with regular php changes for backend build

--- src/router.tsx
+++ src/router.tsx
@@ -1,38 +1,33 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import AppShell from "./components/Layout/AppShell";
 import Child from "./features/child/pages/Child";
 import Donors from "./features/donor/pages/Donor";
 import Lottery from "./features/lottery/pages/Lottery";
 
-
-
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
     {
-        path:"/",
-        element: <AppShell />,
-        children: [
-            {
-                path: "child",
-                element: <Child />
-            },
-            {
-                path: "donors",
-                element: <Donors />
-            },
-            {
-                path: "lottery",
-                element: <Lottery />
-            }
-
-        ],
-
-       
-    }
-  
-],
-// {basename: "/Christmas"}
+      path: "/",
+      element: <AppShell />,
+      children: [
+        {
+          path: "child",
+          element: <Child />,
+        },
+        {
+          path: "donors",
+          element: <Donors />,
+        },
+        {
+          path: "lottery",
+          element: <Lottery />,
+        },
+      ],
+    },
+  ],
+  { basename: "/Christmas" },
 );
-export default function AppRouter(){
-    return <RouterProvider router={router} />
+export default function AppRouter() {
+  return <RouterProvider router={router} />;
 }
